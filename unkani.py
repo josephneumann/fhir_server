6da90fff3259762fdd68cdf6d13573c1e54dd515
Joseph Neumann <joseph.r.neumann@icloud.com>
2018-09-13 07:59:20 -0500

    Enable old style app.run() method for Flask to allow pass-through of errors to Pycharm debugger

--- unkani.py
+++ unkani.py
@@ -3,3 +3,7 @@
 from app.app import create_app
 
 app = create_app()
+
+# Enable old style app.run() method for Flask to allow pass-through of errors to Pycharm debugger
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=True, passthrough_errors=True)
